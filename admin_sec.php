620b8410c4d3f5c221421e4be845139dc5ed8037
Evan Walter <evanwalter@yahoo.com>
2021-07-28 14:01:27 -0500

    Add files via upload

--- admin_sec.php
+++ admin_sec.php
@@ -16,6 +16,7 @@ $dbname = "garage_project";
 
 $current_datetime= date("Y-m-d h:i:s");
  
+$message = "";
 $email = "";
 $customer_id="";
 $vehicle_id="";
@@ -28,6 +29,7 @@ $is_fee_exempt = "0";
 $prohibit_from_entering = "0";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $action = $_POST['action'];	
   // collect value of input field
   $email = $_POST['email'];
   $license = $_POST['license'];
@@ -37,19 +39,33 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $is_fee_exempt = $_POST['is_fee_exempt'];
   $prohibit_from_entering = $_POST['prohibit_from_entering'];
   $result_ret=lookup_customer($email,$license,$state);
-  $customer_id=$result_ret["customer_id"];
   $email = $_POST['email'];
   $state=$result_ret["state"];
   $license=$result_ret["license"];
-  
-  if (($action=="update")&& ($customer_id != "")) {
-	  echo "teatetete" . $customer_id;
-	  update_customer($customer_id,$is_employee,$is_admin,$is_fee_exempt,$prohibit_from_entering);
+  $customer_id=$result_ret["customer_id"];
+
+/*  
+ if (($customer_id == "")&&($email != "")&&($action=="add" )) {
+	  $customer_id=add_customer($email,$license,$state);
+	  if ($customer_id == "") {
+		  $action="update";
+		  $message="";	  
+	  }
   }
-  if ($customer_id != "") {
-	  $action="update";
+  */
+  if (($action=="update")&& ($customer_id != "")) {
+	  update_customer($customer_id,$license,$state,$is_employee,$is_admin,$is_fee_exempt,$prohibit_from_entering);
   }
 
+  if (($customer_id == "")&&($email != "")) {
+	  $action="add";
+	  $message="Record Not Found";
+  }
+  
+  if ($customer_id != "") {
+	   $message="";
+	  $action="update";
+  }
 } 
 
   if (empty($email)) {
@@ -61,6 +77,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
  	echo "State: <input type=\"text\" name=\"state\" value=\"" . $state . "\"><br>";
 	echo "<input type=\"hidden\" name=\"action\" value=\"" . $action . "\">";
 	echo "<input type=\"hidden\" name=\"customer_id\" value=\"" . $customer_id . "\">";
+	echo "<p style=\"color: red;\">". $message  ."</p>";
+  
   
  function lookup_customer($email,$license,$state) {
 	$servername = "127.0.0.1";
@@ -93,7 +111,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	return $result_ret;
 }	
 
- function update_customer($customer_id,$is_employee,$is_admin,$is_fee_exempt,$prohibit_from_entering) {
+
+function add_customer($email,$license,$state) {
+	$servername = "127.0.0.1";
+	$username =   "garageuser"; 
+	$password =  "etA36wq51";  
+	$dbname = "garage_project";
+	
+	$customer_id="";
+	
+	// Create connection
+	$conn = new mysqli("$servername", "$username", "$password", "$dbname");
+	// Check connection
+	if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+	}
+
+	$sql = "INSERT INTO Customer(email) values ('$email');
+			SELECT customer_id FROM Customer where email='$email';
+	";
+	echo $sql;
+	$result = $conn->query($sql);	
+	if ($result->num_rows > 0) {
+	  // output data of each row
+	  while($row = $result->fetch_assoc()) {
+		  $customer_id=  $row["customer_id"];
+	  }
+	}
+	
+	if ($customer_id != "") {
+		$sql = "INSERT INTO  Vehicle (customer_id,licence_number,state)
+				SELECT $customer_id,'$license','$state';";
+		$result = $conn->query($sql);	
+	}
+
+	$conn->close();
+	return $customer_id;
+}	
+
+
+
+ function update_customer($customer_id,$license,$state,$is_employee,$is_admin,$is_fee_exempt,$prohibit_from_entering) {
 	$servername = "127.0.0.1";
 	$username =   "garageuser"; 
 	$password =  "etA36wq51";  
@@ -107,13 +165,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	if ($conn->connect_error) {
 	die("Connection failed: " . $conn->connect_error);
 	}
+	$sql = "UPDATE Vehicle SET customer_id=$customer_id
+			WHERE licence_number='$license' and state='$state';";
+	$result = $conn->query($sql);	
+
+	$sql = "INSERT INTO  Vehicle (customer_id,licence_number,state)
+	        SELECT $customer_id,'$license','$state'
+			WHERE NOT EXITS(select Count(1) from Vehicle where licence_number='$license' and state='$state');";
+	$result = $conn->query($sql);	
+	
 	$sql = "UPDATE Customer SET 
 			is_employee=$is_employee,
 			is_admin=$is_admin,
 			is_fee_exempt=$is_fee_exempt,
 			prohibit_from_entering=$prohibit_from_entering
 			WHERE customer_id=$customer_id;";
-			
 	$result = $conn->query($sql);
 
 	$conn->close();
@@ -128,7 +194,17 @@ if ( $action == "lookup") {
 ?>
    <input id="update" type="submit" value="Lookup"/>
 
-<?php } else {?>
+<?php } else {
+if ( $action == "add") {
+	?>
+	
+	Customer not found
+<br>
+   <input id="add" type="submit" value="Add"/>
+	
+	<?php 
+} else {
+	?>
 <br>
 Is Employee: <select name="is_employee">
    <option value="1">Yes</option>
@@ -152,7 +228,8 @@ Prohibit from entering: <select name="prohibit_from_entering" default="0">
 
 <br>
    <input id="update" type="submit" value="Update Customer"/>
-<?php };?>
+<?php }
+ };?>
 
 
 </form>
