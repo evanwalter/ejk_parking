38997da31d7adf4f2c7509d632cba36a7eb7021c
Evan Walter <evanwalter@yahoo.com>
2021-07-30 20:45:42 -0500

    Add files via upload

--- exitportal.php
+++ exitportal.php
@@ -6,8 +6,9 @@
 
 </head>
 <body>
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
 <!--  in directory  php -S 127.0.0.1:4000  -->
-<!--   http://127.0.0.1:4000/exit.php?camera=1&key=tnlkc6nfe363atedae94h&license=testcar&state=TX   -->
+<!--   http://127.0.0.1:4000/exitportal.php?camera=1&key=tnlkc6nfe363atedae94h&license=testcar&state=TX   -->
 
 <?php
 
@@ -72,7 +73,7 @@ function determine_fee($conn, $license, $state, $current_datetime) {
 	return $fee;
 }	
 
-function end_parking_event($conn, $parking_event_id,$fee_amount,$current_datetime,$payment_id) {
+function end_parking_event($conn, $parking_event_id,$fee_amount,$current_datetime) {
 	$sql = "UPDATE ParkingEvent SET fee_amount=$fee_amount,time_end='$current_datetime'
 	WHERE parking_event_id=$parking_event_id;";
 			   //echo $sql;
@@ -115,7 +116,13 @@ $dbname = "garage_project";
 
 $current_datetime= date("Y-m-d h:i:s");
 
-$reply="{ \"status\": 400,\"message\": \"Unable to enter\" }";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ 	  $parking_event_id=$_POST['parking_event_idh'];
+	  $camera_id = $_POST["camera_id"];
+	  $auth_key = $_POST["auth_key"];
+
+}
 
 // Create connection
 $conn = new mysqli("$servername", "$username", "$password", "$dbname");
@@ -132,6 +139,14 @@ if (!$grant_access==True){
 	die("{ \"status\": 400,\"message\": \"Unable to authenticate\" }");
 }
 
+// IF PAYMENT OPTION WAS OFFERED ATTEMPT TO PAY IT
+if ($parking_event_id != ""){
+ ///////////////////////////////////////////////////
+ ///////////// HERE WE WILL CALL A METHOD TO AUTHENTICATE PAYMENT
+ end_parking_event($conn, $parking_event_id,$fee_amount,$current_datetime);
+	
+}
+
 //  SEARCHING FOR VEHICLE
 $fee_arr=determine_fee($conn, $license, $state,$current_datetime);
 $reply ="";
@@ -141,7 +156,7 @@ $parking_event_id=$fee_arr["parking_event_id"];
 $fee=$fee_arr["fee"];
 $fee_amount=$fee_arr["fee"];
 
-if ($fee=="0.00"){
+if ($fee=="0.00" || $fee==""){
 	echo "<div class=\"container\" ><br>";
 	echo "<p>Thank You.  Have a Nice Day</p>";
 	echo "</div>";
@@ -154,26 +169,41 @@ if ($fee=="0.00"){
 		}
 
 		echo "<div class=\"container\" ><br>";
-
+		
+		echo "<h3>Amount owed: \$$fee_amount.</h3>
+		<h4>Please enter cash below or enter credit card information.</h4><br>";
+		if (count($payment_methods) > 0) {
 		echo "<select name=\"paymentoption\" >";
 		foreach ($payment_methods as &$value) {
 			echo "<option > $value (saved account)</option>";
-		}
+		} 
 		echo "<option>New</option>";
 		echo "</select><br><br><br>";
-		echo "Name on card: <input type=\"text\" name=\"nameoncard\" ><br>";
-		echo "Card Number: <input type=\"text\" name=\"number\" ><br>";
-		echo "Month: <input type=\"text\" name=\"month\" ><br>";
-		echo "Year: <input type=\"text\" name=\"year\" ><br>";
-		echo "accesscode: <input type=\"text\" name=\"accesscode\" ><br><br><br>";
-		echo "<input class=\"btn btn-primary\" type=\"button\" value=\"submit\"   >";
+		}
+		echo "<table>";
+		echo "<tr><td>Name on card: </td><td><input type=\"text\" name=\"nameoncard\" ></td></tr>";
+		echo "<tr><td>Card Number: </td><td><input type=\"text\" name=\"number\" ></td></tr>";
+		//echo "Month: <input type=\"text\" name=\"month\" ><br>";
+		//echo "Year: <input type=\"text\" name=\"year\" ><br>";
+		echo "<tr><td>Expiration (MM/YY): </td><td><select name=\"m1\"><option>0</option><option>1</option><option>2</option></select>";
+		echo "<select name=\"m2\"><option>0</option><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option></select>";
+		echo "/<select name=\"y1\"><option>2</option><option>3</option></select>";
+		echo "<select name=\"y2\"><option>0</option><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option></select>";
+		echo "</td></tr>";
+		echo "<tr><td>code: </td><td><input type=\"text\" name=\"accesscode\" ></td></tr>";
+		echo "</table><br><br>";
+		echo "<input class=\"btn btn-primary\" type=\"submit\" value=\"submit\"   >";
 		echo "</div>";
+		echo "<input type=\"hidden\"  name=\"$parking_event_idh\" value=\"$parking_event_id\">";
+		echo "<input type=\"hidden\"  name=\"camera_id\" value=\"$camera_id\">";
+		echo "<input type=\"hidden\"  name=\"auth_key\" value=\"$auth_key\">";
+		
 }
 
 $conn->close();
 
 
 ?> 
-
+</form>
 </body>
 </html>
